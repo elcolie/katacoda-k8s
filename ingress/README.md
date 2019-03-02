6357ba18f3406aaee1dda28f303f6045fe51f1a1
Sarit Ritwirune <sarit.r@hbot.io>
2019-03-03 01:16:09 +0700

    First commit

---
+++ ingress/README.md
@@ -0,0 +1,8 @@
+# Difficulty: beginner
+Kubernetes have advanced networking capabilities that allow Pods and Services to communicate inside the cluster's network. An Ingress enables inbound connections to the cluster, allowing external traffic to reach the correct Pod.
+
+Ingress enables externally-reachable urls, load balance traffic, terminate SSL, offer name based virtual hosting for a Kubernetes cluster.
+
+In this scenario you will learn how to deploy and configure Ingress rules to manage incoming HTTP requests.
+
+
---
+++ networking/snippet.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export CLUSTER_IP=$(kubectl get services/webapp1-clusterip-svc -o go-template='{{(index .spec.clusterIP)}}')
+echo CLUSTER_IP=$CLUSTER_IP
+curl $CLUSTER_IP:80
+
+
+export CLUSTER_IP=$(kubectl get services/webapp1-clusterip-targetport-svc -o go-template='{{(index .spec.clusterIP)}}')
+echo CLUSTER_IP=$CLUSTER_IP
+curl $CLUSTER_IP:8080
+
+
+export LoadBalancerIP=$(kubectl get services/webapp1-loadbalancer-svc -o go-template='{{(index .status.loadBalancer.ingress 0).ip}}')
+echo LoadBalancerIP=$LoadBalancerIP
+curl $LoadBalancerIP
